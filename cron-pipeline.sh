7c175b64784f8ee2c2cdb4c1bce629dceb490bc9
digidruid <digidruid@digidruid.com>
2024-09-01 23:49:58 +0200

    fix cron-pipeline.sh

--- cron-pipeline.sh
+++ cron-pipeline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo $(date -Iseconds) >> /opt/support/pipeline-$(date -I).log 2>& 
+echo "$(date -Iseconds)" >> /opt/support/pipeline-$(date -I).log 2>&1 
 /home/ubuntu/pipeline.sh >> /opt/support/pipeline-$(date -I).log 2>&1
 
 
