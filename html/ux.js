320e5037bf0a179f3efbae719d0752c364b7135c
Jacques Verryn <jacquesverryn@trustlink.com>
2024-09-03 11:51:44 +0200

    Adding jQuery

---
+++ html/ux.js
@@ -0,0 +1,22 @@
+// usage example: <button onclick="hideday('day1')">Done</button>
+
+function hideday(id) {
+    $(id).fadeToggle();
+}
+
+function showdays() {
+    $("#day1").fadeToggle();
+    $("#day2").fadeToggle();
+    //$(id).fadeToggle()
+}
+
+
+
+function hideday1() {
+    $("#day1").fadeToggle();
+}
+
+function hideday2() {
+    $("#day2").fadeToggle();
+}
+
