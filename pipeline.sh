d09831d62f4c7d42c318707e715ce0732fb7614b
digidruid <digidruid@digidruid.com>
2024-09-03 06:23:23 +0000

    Pipeline updates

--- pipeline.sh
+++ pipeline.sh
@@ -1,8 +1,11 @@
 #!/bin/bash 
 
+echo "$(date -Iseconds) Updating from git"
 cd /home/ubuntu/mocdss/demo1/mcdss-training
+git stash
 git pull
 
+echo "$(date -Iseconds) Deploying into www directory"
 cp -r /home/ubuntu/mocdss/demo1/mcdss-training/html/. /var/www/training/html/.
 
 chgrp -R adm /var/www/training/html
