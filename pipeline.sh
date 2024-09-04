06985bd8bec45a7cd9557b977b7aacca200c7dc1
digidruid <digidruid@digidruid.com>
2024-09-04 05:32:34 +0000

    Moving the deployment

--- pipeline.sh
+++ pipeline.sh
@@ -1,12 +1,12 @@
 #!/bin/bash 
 
 echo "$(date -Iseconds) Updating from git"
-cd /home/ubuntu/mocdss/demo1/mcdss-training
+cd /home/ubuntu/mocdss/mcdss-training
 git stash
 git pull
 
 echo "$(date -Iseconds) Deploying into www directory"
-cp -r /home/ubuntu/mocdss/demo1/mcdss-training/html/. /var/www/training/html/.
+cp -r /home/ubuntu/mocdss/mcdss-training/html/. /var/www/training/html/.
 
 chgrp -R adm /var/www/training/html
 
